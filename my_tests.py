33e5ec89faceef67f015fcc289711fd1ab733ec3
oghodwhy <moena94@hotmail.com>
2017-07-21 00:38:00 -0700

    Moved tests to mytest.py
    returned test files to their real spot and made my own test file

---
+++ my_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import codecs
+import os
+
+from tests.test_mothership_basic import TestMothershipBasic
+from tests.test_worker_basic import TestWorkerBasic
+
+
+if __name__ == "__main__":
+    dostuff = True
+
+
+
+
+
